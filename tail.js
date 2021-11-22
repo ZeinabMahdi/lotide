d70b3c3a1d59475469d88d28e2e5948c37d19c11
Zeinab <58231230+ZeinabMahdi@users.noreply.github.com>
2021-11-22 17:43:50 -0500

    Update tail.js

--- tail.js
+++ tail.js
@@ -13,7 +13,7 @@ const assertEqual = function(actual, expected) {
 
 const tail = function(array) {
   let newArray = [];
-  for (let i = 1; i < array.length; i++) {
+  for (let i = array[1]; i < array.length; i++) {
     newArray.push(i);
   } return newArray;
 };
