cbdaaf955ffa3bdcd2a00291adb42ed72d1f6760
Zeinab <58231230+ZeinabMahdi@users.noreply.github.com>
2021-11-22 18:01:25 -0500

    Update tail.js

--- tail.js
+++ tail.js
@@ -12,10 +12,8 @@ const assertEqual = function(actual, expected) {
 // };
 
 const tail = function(array) {
-  let newArray = [];
-  for (let i = array[1]; i < array.length; i++) {
-    newArray.push(i);
-  } return newArray;
+  let newArray = array.slice(1);
+  console.log(newArray);
 };
 
 // const result = tail(["Hello", "Lighthouse", "Labs"]);
