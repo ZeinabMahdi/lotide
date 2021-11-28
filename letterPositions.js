c89642203dd6b64cdec73c78bce41a1b786f7c19
Zeinab Mahdi <zeinab_750@hotmail.com>
2021-11-28 15:39:41 -0500

    edited

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,20 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i <= arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`💚 Assertion passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑 Assertion failed: ${arr1} !== ${arr2}`);
+  }
+};
+
 const letterPositions = function(sentence) {
   const results = {};
   for (let index = 0; index < sentence.length; index++) {
@@ -12,4 +29,6 @@ const letterPositions = function(sentence) {
   }
   return results;
 };
-console.log(letterPositions("lighthouse in the house"));
+
+console.log(assertArraysEqual(letterPositions("lighthouse in the house").h, [3]));
+console.log(assertArraysEqual(letterPositions("hello").e, [1]));
