51dd96b1b8b713665f7c6335fbf65bacfe726cd5
Zeinab Mahdi <zeinab_750@hotmail.com>
2021-11-29 15:59:53 -0500

    adding index & modifications

---
+++ index.js
@@ -0,0 +1,32 @@
+// index.js
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+// const without = require('./without');
+// const assertEqual = require('./assertEqual');
+// const findKey = require('./findKey');
+// const countOnly = require('./countOnly');
+// const eqArrays = require('./eqArrays');
+// const map = require('./map');
+// const middle = require('./middle');
+// const assertArraysEqual = require('./assertArraysEqual');
+// const letterPositions = require('./letterPositions');
+// const findKeyByValue = require('./findKeyByValue');
+// const countLetters = require('./countLetters');
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  // without: without,
+  // assertEqual: assertEqual,
+  // findKey: findKey,
+  // countOnly: countOnly,
+  // eqArrays: eqArrays,
+  // map: map,
+  // middle: middle,
+  // assertArraysEqual: assertArraysEqual,
+  // letterPositions: letterPositions,
+  // findKeyByValue: findKeyByValue,
+  // countLetters: countLetters
+};
--- without.js
+++ without.js
@@ -27,10 +27,4 @@ const without = function(source, itemsToRemove) {
   } return newArray;
 };
 
-console.log(without([1,2,3], [1,]));
-console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
-
-const words = ["hello", "world", "lighthouse"];
-// console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+module.exports = without;
