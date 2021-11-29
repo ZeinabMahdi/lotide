d0efae5fd65ec7a34038f62354f2e99cb2e4e98c
Zeinab Mahdi <zeinab_750@hotmail.com>
2021-11-29 12:34:59 -0500

    refactor

---
+++ test/eqArraysTest.js
@@ -0,0 +1,2 @@
+// const assertEqual = require('../assertEqual');
+// const head = require('../eqArrays');
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,5 @@
 const assertEqual = require('../assertEqual');
-const head = require('../tail');
+const tail = require('../tail');
 
 // TEST CODE
 const words = ["Yo Yo", "Lighthouse", "Labs"];
