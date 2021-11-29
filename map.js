375f61c3e582b3fb015d2aba75c323c86a99a9eb
Zeinab Mahdi <zeinab_750@hotmail.com>
2021-11-28 20:37:47 -0500

    debg

--- map.js
+++ map.js
@@ -1,17 +1,20 @@
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i <= arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length != arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
       return false;
     }
   }
   return true;
 };
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`💚 Assertion passed: ${arr1} === ${arr2}`);
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`💚 Assertion passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑 Assertion failed: ${arr1} !== ${arr2}`);
+    console.log(`🛑 Assertion failed: ${actual} !== ${expected}`);
   }
 };
 
